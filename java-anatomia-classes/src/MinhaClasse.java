dddce4611f0fea21475c9916e3c2f3e44f2c2e28
QuintanaDan <daniquints17@gmail.com>
2025-01-22 06:09:03 -0300

    metodo de juncao de string

--- java-anatomia-classes/src/MinhaClasse.java
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -2,8 +2,16 @@ public class MinhaClasse {
 
     public static void main (String [] args) {
 
-        System.out.print ("Olá turma, sejam bem-vindos");
-        
+        String primeiroNome = "Daniel";
+        String segundoNome = "Quintana";
+
+        String nomeCompleto = nomeCompleto (primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome) {
+        return "Resultado do metodo: " + primeiroNome.concat(" ").concat(segundoNome);
     }
     
 }
